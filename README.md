1dd9d0c0535d41201fd863c3150e897390dff91a
gidlov <info@gidlov.com>
2013-09-28 00:42:57 +0200

    Publish

---
+++ README.md
@@ -0,0 +1,61 @@
+A Gallery plugin for the micro CMS Pico
+=====================
+You may find more info on [gidlov.com/code/gallery](http://gidlov.com/code/gallery)
+
+##About##
+
+You can have as many galleries you want, with customizable markup. You can view all on the same page or different pages, and you can choose where on the page. Not so sophisticated, but it gets the job done.
+
+###Setup###
+As we make use of the excellent [Image](http://intervention.olivervogel.net/) class we have to update the `require` key of `composer.json` and add the following:
+
+	"intervention/image": "dev-master"
+
+Run the `composer update` comand.
+
+Create two folders, one for your pre-scaled large images and one for the thumbnails. For example:
+
+	content/gallery/fullzise
+	content/gallery/thumbnails
+	...
+	content/index.md
+
+Copy your photos to the fullsize-folder.
+
+Add a `gallery` array to your `config` variable and modify according to your needs.
+
+	$config['gallery'] = array(
+		'my_gallery' => array(						// Add %my_gallery% some place in your page.
+			'page' => 'index',						// The page you want to view the gallery.
+			'image_path' => 'gallery/fullsize',		// Original Images.
+			'thumb_path' => 'gallery/thumbnails',	// Must be a empty separate folder.
+			'flush' => '47b71a856274ac45',			// A unique string to delete and regenerate thumbnails.
+
+			// These are possible, but not mandatory...
+			'thumb_size' => array(200, 200),
+			'before_gallery' => '',
+			'after_gallery' => '',
+			'before_thumbnail' => '',
+			'after_thumbnail' => '',
+			'thumbnail_link_class' => '',
+			'thumbnail_image_class' => '',
+			'exclude' => array(),					// Exclude file name(s).
+			'sort_by' => '',	 					// Sort by empty or 'random'.
+			'order_by' => '', 						// Order by ampty or 'reverse'.
+	),
+
+Save your `config.php` and enter the URL `http//example.com/index/my_gallery/flush/47b71a856274ac45` to generate the thumbnails.
+
+Add `%my_gallery%` to your `index.md` file, or wherever you want.
+
+Visit http//example.com/index to see your new gallery.
+
+####Done!####
+
+##Requirements##
+
+ - PHP 5.3
+
+##License##
+
+Gallery is released under [LGPL](http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
---
+++ gallery.php
@@ -0,0 +1,178 @@
+<?php use Intervention\Image\Image;
+
+/**
+ * A Gallery plugin for the micro CMS Pico.
+ *
+ * @author Klas Gidlöv
+ * @link http://gidlov.com/code
+ * @license LGPL
+ */
+
+define('GALLERY_PRE_THUMB_NAME', 'thumb_');
+define('GALLERY_FLUSH_URL', 'flush');
+define('GALLERY_DIR', str_replace(ROOT_DIR, '', CONTENT_DIR));
+
+class Gallery {
+
+	private $url;
+	private $config;
+	private $gallery;
+	private $image;
+	private $request;
+	private $current;
+	private $file;
+
+	/**
+	* Load config variables before they are loaded automatically.
+	*/
+	public function __construct() {
+		global $config;
+		@include_once(ROOT_DIR .'config.php');
+		if (!isset($config['gallery'])) {
+			return;
+		}
+		$this->gallery = $config['gallery'];
+	}
+
+	/**
+	* Examines the URL address.
+	*/
+	public function request_url(&$url) {
+		if (!isset($this->gallery)) {
+			return;
+		}
+		$this->url = $url;
+		$this->url = ($this->url == '') ? 'index' : $this->url;
+		$this->request = array();
+		foreach (array_keys($this->gallery) as $gallery_name) {
+			if ($this->gallery[$gallery_name]['page'] == $this->url) {
+				$this->request[] = $gallery_name;
+			}
+			$url_strip = str_replace($this->gallery[$gallery_name]['page'].'/', '', $this->url);
+			$url_part = explode('/', $url_strip);
+			if ($url_part[0] == $gallery_name) {
+				if (isset($url_part[1]) && $url_part[1] == GALLERY_FLUSH_URL && isset($url_part[2]) && $url_part[2] == $this->gallery[$gallery_name]['flush']) {
+					$this->flush($gallery_name);
+					$url = '';
+				} elseif (isset($url_part[1]) && is_file(GALLERY_DIR.$this->gallery[$gallery_name]['image_path'].'/'.$url_part[1])) {
+					$this->current = $gallery_name;
+					$this->image = GALLERY_DIR.$this->gallery[$gallery_name]['image_path'].'/'.$url_part[1];
+					unset($url_part[count($url_part)-1]);
+					$this->file = CONTENT_DIR.$this->gallery[$gallery_name]['page'].CONTENT_EXT;
+				}
+			}
+		}
+	}
+	
+	/**
+	* If asked for a picture or a flush request, redirect from error 404 page to your gallery page.
+	*/
+	public function before_load_content(&$file) {
+		if ($this->file) {
+			$file = $this->file;
+		}
+	}
+
+	/**
+	* Save the real config variable.
+	*/
+	public function config_loaded(&$settings) {
+		$this->config = $settings;
+	}
+
+	/**
+	* Checking whether your gallery_id exists in your markdown and replaces in such a case.
+	*/
+	public function after_render(&$output) {
+		if ($this->request) {
+			// Get the gallery.
+			foreach ($this->request as $gallery_name) {
+				if (preg_match('/%'.$gallery_name.'%/mi', $output, $match) && $match[0]) {
+					$output = str_replace('%'.$gallery_name.'%', $this->markup($gallery_name), $output);
+				}
+			}
+		} elseif ($this->image && $this->current) {
+			// Get the image.
+			foreach (array_keys($this->gallery) as $gallery_name) {
+				$replace = $gallery_name == $this->current ? $this->markup($this->current) : '';
+				$output = str_replace('%'.$gallery_name.'%', $replace, $output);
+			}
+		}
+	}
+
+	/**
+	* Deletes thumbnails and generate new ones.
+	*/
+	protected function flush($name) {
+		$image_path = GALLERY_DIR.$this->gallery[$name]['image_path'];
+		$thumb_path = GALLERY_DIR.$this->gallery[$name]['thumb_path'];
+		if (!is_dir($image_path)) {
+			return;
+		}
+		$files = glob($thumb_path.DIRECTORY_SEPARATOR.'*');
+		foreach($files as $file) {
+			// Delete all files in the thumbnail folder.
+			if (is_file($file)) {
+				unlink($file);
+			}
+		}
+		foreach(glob($image_path.'/{*.jpg,*.png,*.gif}', GLOB_BRACE) as $image) {
+			// Generate new thumbnails.
+			$size = isset($this->gallery[$name]['thumb_size']) ? $this->gallery[$name]['thumb_size'] : array(200, 200);
+			$file_name = basename($image);
+			$image = Image::make($image)
+				->grab($size[0], $size[1])
+				->save($thumb_path.DIRECTORY_SEPARATOR.GALLERY_PRE_THUMB_NAME.$file_name, 80);
+		}
+	}
+
+	/**
+	* Generates HTML markup for a gallery or a picture.
+	*/
+	protected function markup($name) {
+		$return = '';
+		$image_path = GALLERY_DIR.$this->gallery[$name]['image_path'];
+		$thumb_path = GALLERY_DIR.$this->gallery[$name]['thumb_path'];
+		if ($this->image) {
+			// Markup for a image
+			$before = (isset($this->gallery[$name]['before_image'])) ? $this->gallery[$name]['before_image'] : "\t\t\t\t".'<div class="thumbnail">'."\n";
+			$after = (isset($this->gallery[$name]['after_image'])) ? $this->gallery[$name]['after_image'] : "\t\t\t\t".'</div>'."\n";
+			
+			$return .= $before;
+			$return .= "\t\t\t\t".'<img src="'.$this->config['base_url'].'/'.$this->image.'" alt="..." class="attachment-medium">'."\n";
+			$return .= $after;
+			return $return;
+		} else {
+			// Markup for a gallery.
+			$before_gallery = (isset($this->gallery[$name]['before_gallery'])) ? $this->gallery[$name]['before_gallery'] : "\t\t\t\t".'<div class="row">'."\n";
+			$after_gallery = (isset($this->gallery[$name]['after_gallery'])) ? $this->gallery[$name]['after_gallery'] : "\t\t\t\t".'</div>'."\n";
+			$before_thumbnail = (isset($this->gallery[$name]['before_thumbnail'])) ? $this->gallery[$name]['before_thumbnail'] : "\t\t\t\t\t".'<div class="col-lg-3 col-md-4 col-xs-6 thumb">'."\n";
+			$after_thumbnail = (isset($this->gallery[$name]['after_thumbnail'])) ? $this->gallery[$name]['after_thumbnail'] : "\t\t\t\t\t".'</div>'."\n";
+			$thumbnail_link_class = (isset($this->gallery[$name]['thumbnail_link_class'])) ? $this->gallery[$name]['thumbnail_link_class'] : "thumbnail";
+			$thumbnail_image_class = (isset($this->gallery[$name]['thumbnail_image_class'])) ? $this->gallery[$name]['thumbnail_image_class'] : "img-responsive";
+			
+			$return .= $before_gallery;
+			$gallery = glob($image_path.'/{*.jpg,*.png,*.gif}', GLOB_BRACE);
+			if (isset($this->gallery[$name]['sort_by']) && $this->gallery[$name]['sort_by'] == 'random') {
+				shuffle($gallery);
+			}
+			if (isset($this->gallery[$name]['order_by']) && $this->gallery[$name]['order_by'] == 'reverse') {
+				array_reverse($gallery);
+			}
+			foreach($gallery as $image) {
+				if (isset($this->gallery[$name]['exclude']) && in_array(substr($image, strrpos($image, '/')+1), $this->gallery[$name]['exclude'])) {
+					// Skip files in the exclude array.
+					continue;
+				}
+				$file_name = basename($image);
+				$return .= $before_thumbnail;
+				$return .= "\t\t\t\t\t\t".'<a href="'.$this->config['base_url'].'/'.$this->url.'/'.$name.'/'.$file_name.'" class="'.$thumbnail_link_class.'">'."\n";
+				$return .= "\t\t\t\t\t\t\t".'<img src="'.$this->config['base_url'].'/'.$thumb_path.'/'.GALLERY_PRE_THUMB_NAME.$file_name.'" alt="..." class="'.$thumbnail_image_class.'">'."\n";
+				$return .= "\t\t\t\t\t\t".'</a>'."\n";
+				$return .= $after_thumbnail;
+			}
+			$return .= $after_gallery;
+			return $return;
+		}
+	}
+}
